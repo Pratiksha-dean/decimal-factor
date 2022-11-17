8cf457d1533910954b5dc314fc09f0879ee51516
deansneha <sneha@deaninfotech.com>
2022-11-17 11:13:31 +0530

    loader work

---
+++ src/component/loader.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import LoadingSpin from "react-loading-spin";
+
+
+export default function Loaderspinner() {
+  return (
+   
+     <div className="no-data-panel">
+       
+     <div className="no-data">
+        <LoadingSpin
+            duration="2s"
+            width="5px"
+            timingFunction="ease-in-out"
+            direction="alternate"
+            size="30px"
+            primaryColor="rgb(0, 96, 144) rgb(0, 126, 255)"
+            secondaryColor=" red rgb(32 149 207)"
+            numberOfRotationsInAnimation={2}
+         />
+     
+     </div>
+     </div>
+  )
+}
